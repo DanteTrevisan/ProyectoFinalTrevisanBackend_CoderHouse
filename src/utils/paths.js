da616145c5d1ff05be4a5ac6c104f007c66db7e6
Dante Trevisan <dante.trevisan248@gmail.com>
2025-03-17 12:09:05 -0300

    agregadas dependencias

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -4,6 +4,7 @@ import path from "path";
 const directoryUrl = new URL("../../", import.meta.url);
 
 export const rootPath = url.fileURLToPath(directoryUrl);
+console.log(rootPath)
 export const productsPath = rootPath + "src\\data\\products.json"
 export const cartsPath = rootPath + "src\\data\\carts.json"
 export const productsImagesPath = rootPath + "src\\assets\\images\\products"
